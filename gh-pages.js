7cb76c407e11a57c4246f91fe79d017f423dd4ac
braveheartwilliam <braveheartwilliam@hotmail.com>
2024-01-01 02:18:55 -0500

    Updating trying to get to build afor github pages

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/braveheartwilliam/MasteringSvelteFramework2023.git', // Update to point to your repository
+        user: {
+            name: 'William Wallace', // update to use your name
+            email: 'braveheartwilliam@protonmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
